f8a2b84b04a6880635f9b4b857ea73a9ab26135a
priti <pritiranabhat8@gmail.com>
2024-12-04 21:43:22 +0545

    day1

---
+++ day1.py
@@ -0,0 +1,14 @@
+print("Hello World")
+list = ["Apple", "Banana", "Grape"]
+print(list)
+print(len(list))
+list = ["hbd", 3, True, "male"]
+print(list)
+print(type(list))
+thislist = list(("Apple", "grape", "cherry"))
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
